74d6a478f203e0a182c9bd825a12bce48ce66b22
Andrey Vlasov <lion32211@yandex.ru>
2023-12-06 21:58:54 +0100

    Removed unused interface

--- AutomationFramework.Common/Abstractions/IDefaultVariables.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutomationFramework.Common.Abstractions;
-
-public interface IDefaultVariables
-{
-    public string Log { get; }
-    public string Results { get; }
-    public string Solution { get; }
-    public string Config { get; }
-}
