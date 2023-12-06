145e7b3d4d6f096be8b30c3a7204f585e8e81a72
Andrey Vlasov <lion32211@yandex.ru>
2023-12-06 22:06:04 +0100

    Add DI container

---
+++ AutomationFramework.Core/Dependencies/DIContainer.cs
@@ -0,0 +1,33 @@
+﻿using AutomationFramework.Common.Abstractions;
+using AutomationFramework.Common.Variables;
+using AutomationFramework.Common.Reports;
+using AutomationFramework.Core.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using AutomationFramework.Core.Selenium.WebDriverFactory;
+using AutomationFramework.Core.Selenium;
+
+namespace AutomationFramework.Core.Dependencies;
+
+public class DIContainer
+{
+    public static IServiceCollection serviceCollection;
+
+    public static IServiceProvider ConfigureServices()
+    {
+        if (serviceCollection == null)
+        {
+            serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<DefaultVariables>();
+            serviceCollection.AddSingleton<ILogging, ConsoleLogger>(); // [ConsoleLogger/FileLogger/SpecflowLogger/]
+            serviceCollection.AddSingleton<TestRunConfiguration>();
+            serviceCollection.AddSingleton<INamedBrowserFactory, ChromeDriverFactory>();
+            serviceCollection.AddSingleton<INamedBrowserFactory, FirefoxDriverFactory>();
+            serviceCollection.AddSingleton<INamedBrowserFactory, RemoteChromeDriverFactory>();
+            serviceCollection.AddSingleton<INamedBrowserFactory, RemoteFirefoxDriverFactory>();
+            serviceCollection.AddSingleton<IWebDriverWrapper, WebDriverWrapper>();
+
+        }
+
+        return serviceCollection.BuildServiceProvider();
+    }
+}
