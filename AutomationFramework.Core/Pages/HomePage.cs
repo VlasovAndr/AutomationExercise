c8759ee88ad407c6c51e89a054237cf8e98d27af
Andrey Vlasov <lion32211@yandex.ru>
2023-12-14 20:44:35 +0100

    Add HomePage POM

---
+++ AutomationFramework.Core/Pages/HomePage.cs
@@ -0,0 +1,27 @@
+﻿using AutomationFramework.Common.Abstractions;
+using AutomationFramework.Core.Configuration;
+
+namespace AutomationFramework.Core.Pages;
+
+public class HomePage : PageBase
+{
+    private const string PAGE_NAME = "Home Page";
+    public readonly Header header;
+
+    public HomePage(IWebDriverWrapper browser, ILogging log, TestRunConfiguration config, Header header)
+        : base(browser, log, config)
+    {
+        this.header = header;
+    }
+
+    public void Open()
+    {
+        browser.NavigateToUrl(BaseUrl);
+        log.Information($"|{PAGE_NAME}| {PAGE_NAME} is opened");
+    }
+
+    public bool IsPageOpen()
+    {
+        return header.IsVisible() && GetTitle().Equals("Automation Exercise");
+    }
+}
