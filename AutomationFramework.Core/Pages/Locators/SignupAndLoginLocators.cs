0481291948d548333cce8c45ff42b3b8b8fb00fb
Andrey Vlasov <lion32211@yandex.ru>
2023-12-14 20:45:00 +0100

    Add SignupAndLoginPage POM with locators

---
+++ AutomationFramework.Core/Pages/Locators/SignupAndLoginLocators.cs
@@ -0,0 +1,16 @@
+﻿namespace AutomationFramework.Core.Pages.Locators;
+
+public class SignupAndLoginLocators
+{
+    public string SignupForm => $".//div[@class = 'signup-form']";
+    public string SignupFormTitle => $"{SignupForm}/h2";
+    public string SignupNameField => $"{SignupForm}//input[@data-qa = 'signup-name']";
+    public string SignupEmailField => $"{SignupForm}//input[@data-qa = 'signup-email']";
+    public string SignupBtn => $".//button[@data-qa = 'login-button']";
+
+    public string LoginForm => $".//div[@class = 'login-form']";
+    public string LoginFormTitle => $"{LoginForm}/h2";
+    public string LoginEmailField => $"{LoginForm}//input[@data-qa = 'login-email']";
+    public string LoginPasswordField => $"{LoginForm}//input[@data-qa = 'login-password']";
+    public string LoginBtn => $".//button[@data-qa = 'signup-button']";
+}
