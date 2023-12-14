f12d29a32d0360873683e9e2b239944201a98ce5
Andrey Vlasov <lion32211@yandex.ru>
2023-12-14 20:23:01 +0100

    Add AccountInfo class

---
+++ AutomationFramework.Common/Models/AccountInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace AutomationFramework.Common.Models;
+
+public class AccountInfo
+{
+    public string Gender { get; }
+    public string Name { get; }
+    public string Email { get; }
+    public string Password { get; }
+    public DateTime DateOfBirth { get; }
+    public bool IsNewsletter { get; }
+    public bool IsSpecialOffers { get; }
+
+    public AccountInfo(string gender, string name, string email, string password, DateTime dateOfBirth, bool newsletter, bool specialOffers)
+    {
+        Gender = gender;
+        Name = name;
+        Email = email;
+        Password = password;
+        DateOfBirth = dateOfBirth;
+        IsNewsletter = newsletter;
+        IsSpecialOffers = specialOffers;
+    }
+}
