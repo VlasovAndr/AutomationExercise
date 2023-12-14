363597a20086e7f3283d7aa2d3f08017ca0cb2bb
Andrey Vlasov <lion32211@yandex.ru>
2023-12-14 20:42:30 +0100

    Add User model

---
+++ AutomationFramework.Common/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomationFramework.Common.Models;
+
+public class User
+{
+    public AccountInfo Account { get; }
+    public AddressInfo Address { get; }
+
+    public User(AccountInfo accountInfo, AddressInfo addressInfo)
+    {
+        Account = accountInfo;
+        Address = addressInfo;
+    }
+}
