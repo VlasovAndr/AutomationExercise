6c891e0f23eccde627a6eff41ff4781a42d55ae3
Andrey Vlasov <lion32211@yandex.ru>
2023-12-17 19:09:27 +0100

    Add DataGeneratorService to generate random user

---
+++ AutomationFramework.Common/Services/DataGeneratorService.cs
@@ -0,0 +1,42 @@
+﻿using AutomationFramework.Common.Models;
+using Bogus;
+
+namespace AutomationFrameworkCommon.Services;
+
+public class DataGeneratorService
+{
+    private readonly Faker _faker;
+
+    public DataGeneratorService()
+    {
+        _faker = new Faker();
+    }
+
+    public User GenerateRandomUser(bool? newsletterInput = null, bool? specialOffersInput = null)
+    {
+        var accountInfo = new AccountInfo(
+            gender: _faker.PickRandom<Gender>().ToString(),
+            name: _faker.Name.FirstName(),
+            email: _faker.Internet.Email(),
+            password: _faker.Internet.Password(),
+            dateOfBirth: _faker.Person.DateOfBirth,
+            newsletter: newsletterInput == null ? _faker.Random.Bool() : (bool)newsletterInput,
+            specialOffers: specialOffersInput == null ? _faker.Random.Bool() : (bool)specialOffersInput
+            );
+
+        var addressInfo = new AddressInfo(
+            firstName: _faker.Person.FirstName,
+            lastName: _faker.Person.LastName,
+            company: _faker.Company.CompanyName(),
+            address: _faker.Address.FullAddress(),
+            address2: _faker.Address.FullAddress(),
+            country: _faker.PickRandom<Country>().ToString().Replace("_", " "),
+            state: _faker.Address.State(),
+            city: _faker.Address.City(),
+            zipcode: _faker.Address.ZipCode(),
+            mobileNumber: _faker.Person.Phone
+            );
+
+        return new User(accountInfo, addressInfo);
+    }
+}
