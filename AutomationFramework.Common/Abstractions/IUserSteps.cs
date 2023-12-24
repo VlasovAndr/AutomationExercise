2e50ab54f0ec9e1d299ab2f3b04362f55bf0284c
Andrey Vlasov <lion32211@yandex.ru>
2023-12-24 16:53:20 +0100

    Add UserUISteps

---
+++ AutomationFramework.Common/Abstractions/IUserSteps.cs
@@ -0,0 +1,8 @@
+﻿using AutomationFramework.Common.Models;
+
+namespace AutomationFramework.Common.Abstractions;
+
+public interface IUserSteps
+{
+    void RegisterUser(User user);
+}
---
+++ AutomationFramework.Core/Steps/UserUISteps.cs
@@ -0,0 +1,33 @@
+﻿using AutomationFramework.Common.Abstractions;
+using AutomationFramework.Common.Models;
+using AutomationFramework.Core.Pages;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutomationFramework.Core.Steps;
+
+public class UserUISteps : IUserSteps
+{
+    public IServiceProvider container;
+    private readonly SignupAndLoginPage signupAndLoginPage;
+    private readonly SignupPage signupPage;
+
+    public UserUISteps(IServiceProvider container)
+    {
+        this.container = container;
+        signupAndLoginPage = container.GetRequiredService<SignupAndLoginPage>();
+        signupPage = container.GetRequiredService<SignupPage>();
+    }
+
+    public void RegisterUser(User user)
+    {
+        signupAndLoginPage.Open();
+        signupAndLoginPage.FillSignupForm(user.Account.Name, user.Account.Email);
+        signupAndLoginPage.ClickOnSignUpBtn();
+
+        signupPage.FillAccountInfoForm(user.Account);
+        signupPage.FillAddressInfoForm(user.Address);
+        signupPage.ClickOnCreateAccountBtn();
+        signupPage.ClickOnContinueBtn();
+        signupPage.Close();
+    }
+}
