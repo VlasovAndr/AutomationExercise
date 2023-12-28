9668fc047d8942c4ac7dfb339576f38ce27a3973
Andrey Vlasov <lion32211@yandex.ru>
2023-12-28 22:57:38 +0100

    Add BaseApiRequestService

---
+++ AutomationFramework.Common/Services/API/BaseApiRequestService.cs
@@ -0,0 +1,31 @@
+﻿using AutomationFramework.Common.Abstractions;
+
+namespace AutomationFramework.Common.Services.API;
+
+public class BaseApiRequestService
+{
+    private readonly APIClient apiClient;
+    protected readonly ILogging log;
+
+    public BaseApiRequestService(APIClient apiClient, ILogging log)
+    {
+        this.apiClient = apiClient;
+        this.log = log;
+    }
+
+    public Task<HttpResponseMessage> PerformPostFormData(string url, FormUrlEncodedContent content)
+    {
+        var response = apiClient.PostAsync(url, content);
+        response.Result.EnsureSuccessStatusCode();
+
+        return response;
+    }
+
+    public Task<HttpResponseMessage> PerformDeleteFormData(string url, FormUrlEncodedContent content)
+    {
+        var response = apiClient.SendDeleteRequest(url, content);
+        response.Result.EnsureSuccessStatusCode();
+
+        return response;
+    }
+}
