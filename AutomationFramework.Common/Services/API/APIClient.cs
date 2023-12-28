45349537c577945d17e804b180ea996bb6898c26
Andrey Vlasov <lion32211@yandex.ru>
2023-12-28 22:57:20 +0100

    Add APIClient

---
+++ AutomationFramework.Common/Services/API/APIClient.cs
@@ -0,0 +1,43 @@
+﻿namespace AutomationFramework.Common.Services.API;
+
+public class APIClient : IDisposable
+{
+    private readonly Lazy<HttpClient> httpClientService;
+    private HttpClient HttpClient => httpClientService.Value;
+
+    public APIClient()
+    {
+        httpClientService = new Lazy<HttpClient>(CreateHttpClient, true);
+    }
+
+    private HttpClient CreateHttpClient()
+    {
+        var client = new HttpClient();
+        return client;
+    }
+
+    public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent httpContent)
+    {
+        return HttpClient.PostAsync(requestUri, httpContent);
+    }
+
+    public Task<HttpResponseMessage> SendDeleteRequest(string requestUri, HttpContent httpContent)
+    {
+        var request = new HttpRequestMessage
+        {
+            Content = httpContent,
+            Method = HttpMethod.Delete,
+            RequestUri = new Uri(requestUri)
+        };
+
+        return HttpClient.SendAsync(request);
+    }
+
+    public void Dispose()
+    {
+        if (httpClientService.IsValueCreated)
+        {
+            HttpClient.Dispose();
+        }
+    }
+}
