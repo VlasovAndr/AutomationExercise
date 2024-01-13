c08c74d1d2f419249c4b4d6084681f79dadd22bb
Andrey Vlasov <lion32211@yandex.ru>
2024-01-13 18:46:32 +0100

    Add get and put methods in BaseApiRequestService

--- AutomationFramework.Common/Services/API/BaseApiRequestService.cs
+++ AutomationFramework.Common/Services/API/BaseApiRequestService.cs
@@ -1,4 +1,6 @@
 ﻿using AutomationFramework.Common.Abstractions;
+using AutomationFramework.Common.Services.API.Responses;
+using System.Text.Json;
 
 namespace AutomationFramework.Common.Services.API;
 
@@ -13,9 +15,28 @@ public class BaseApiRequestService
         this.log = log;
     }
 
+    public Task<HttpResponseMessage> PerformGet(string url, Dictionary<string, string> parameters)
+    {
+        var queryString = string.Join("?", parameters.Select(kv => $"{kv.Key}={kv.Value}"));
+        var urlWithParam = $"{url}?{queryString}";
+
+        var response = apiClient.GetAsync(urlWithParam);
+        response.Result.EnsureSuccessStatusCode();
+
+        return response;
+    }
+
     public Task<HttpResponseMessage> PerformPostFormData(string url, FormUrlEncodedContent content)
     {
         var response = apiClient.PostAsync(url, content);
+        response.Result.EnsureSuccessStatusCode();
+
+        return response;
+    }
+
+    public Task<HttpResponseMessage> PerformPutFormData(string url, FormUrlEncodedContent content)
+    {
+        var response = apiClient.PutAsync(url, content);
         response.Result.EnsureSuccessStatusCode();
 
         return response;
@@ -27,5 +48,14 @@ public class BaseApiRequestService
         response.Result.EnsureSuccessStatusCode();
 
         return response;
+    }
+
+    public T HandleResponseMessage<T>(string response)
+        where T : BaseAPIResponse
+    {
+        var a = new JsonSerializerOptions();
+        a.IncludeFields = false;
+        var result = JsonSerializer.Deserialize<T>(response, a);
+        return result;
     }
 }
