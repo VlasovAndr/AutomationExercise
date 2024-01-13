6af55e0b707984dcf85c4240da9f436b5d07cc0c
Andrey Vlasov <lion32211@yandex.ru>
2024-01-13 19:12:11 +0100

    Refactor AutomationExerciseAPI tests

--- AutomationExerciseAPI.Tests/AutomationExerciseAPITests.cs
+++ AutomationExerciseAPI.Tests/AutomationExerciseAPITests.cs
@@ -1,10 +1,9 @@
-﻿using AutomationFramework.Common.Services.API;
-using AutomationFrameworkCommon.Services;
+﻿using AutomationFramework.Common.Services;
+using AutomationFramework.Common.Services.API;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
-using System.Net;
 
 namespace AutomationExerciseAPI.Tests;
 
@@ -14,10 +13,12 @@ namespace AutomationExerciseAPI.Tests;
 public class AutomationExerciseAPITests : TestBase
 {
     private readonly UserAPIService userAPIService;
+    private readonly DataGeneratorService generatorService;
 
     public AutomationExerciseAPITests()
     {
         userAPIService = container.GetRequiredService<UserAPIService>();
+        generatorService = container.GetRequiredService<DataGeneratorService>();
     }
 
     [Test, Property("TMSId", "API 11"), Description("POST To Create/Register User Account")]
@@ -27,21 +28,53 @@ public class AutomationExerciseAPITests : TestBase
 
         var response = userAPIService.RegisterUserAccount(user);
 
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        var result = response.Content.ReadAsStringAsync().Result;
-        result.Should().Contain("User created!");
+        response.StatusCode.Should().Be(201);
+        response.Message.Should().Be("User created!");
     }
 
     [Test, Property("TMSId", "API 12"), Description("DELETE METHOD To Delete User Account")]
     public void DeleteUserAccount()
     {
-        var user = new DataGeneratorService().GenerateRandomUser();
+        var user = generatorService.GenerateRandomUser();
         userAPIService.RegisterUserAccount(user);
 
         var response = userAPIService.DeleteUserAccount(user.Account.Email, user.Account.Password);
 
-        var result = response.Content.ReadAsStringAsync().Result;
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        result.Should().Contain("Account deleted!");
+        response.StatusCode.Should().Be(200);
+        response.Message.Should().Be("Account deleted!");
+    }
+
+    [Test, Property("TMSId", "API 13"), Description("PUT METHOD To Update User Account")]
+    public void UpdateUserAccount()
+    {
+        var user = generatorService.GenerateRandomUser();
+        userAPIService.RegisterUserAccount(user, false);
+        user.Address.Address = "New Address";
+        user.Account.IsNewsletter = true;
+        user.Account.IsSpecialOffers = true;
+
+        var response = userAPIService.UpdateUserAccount(user);
+
+        response.StatusCode.Should().Be(200);
+        response.Message.Should().Be("User updated!");
+    }
+
+    [Test, Property("TMSId", "API 14"), Description("GET user account detail by email")]
+    public void GetUserAccountDetailByEmail()
+    {
+        var user = generatorService.GenerateRandomUser();
+        userAPIService.RegisterUserAccount(user);
+
+        var response = userAPIService.GetUserAccountDetail(user.Account.Email);
+
+        response.StatusCode.Should().Be(200);
+        response.User.FirstName.Should().Be(user.Address.FirstName);
+        response.User.LastName.Should().Be(user.Address.LastName);
+        response.User.Address1.Should().Be(user.Address.Address);
+        response.User.Address2.Should().Be(user.Address.Address2);
+        response.User.Country.Should().Be(user.Address.Country);
+        response.User.City.Should().Be(user.Address.City);
+        response.User.State.Should().Be(user.Address.State);
+        response.User.Zipcode.Should().Be(user.Address.Zipcode);
     }
 }
