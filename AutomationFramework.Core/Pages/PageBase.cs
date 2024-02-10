10c794aaf0ef74029ad8792731537eab7028d287
Andrey Vlasov <lion32211@yandex.ru>
2024-02-10 18:11:39 +0100

    Refactor PageBase, add reporting

--- AutomationFramework.Core/Pages/PageBase.cs
+++ AutomationFramework.Core/Pages/PageBase.cs
@@ -9,15 +9,17 @@ public abstract class PageBase
     protected readonly ILogging log;
     protected string BaseUrl => config.TargetEnvironment.Url;
     protected virtual string PageName { get; }
-    protected virtual string PageUrl { get; }
+    protected virtual string PageUrl => BaseUrl;
 
+    private ITestReporter reporter;
     private readonly TestRunConfiguration config;
 
-    public PageBase(IWebDriverWrapper browser, ILogging log, TestRunConfiguration config)
+    public PageBase(IWebDriverWrapper browser, ILogging log, TestRunConfiguration config, ITestReporter reporter)
     {
         this.browser = browser;
         this.log = log;
         this.config = config;
+        this.reporter = reporter;
     }
 
     public void Open()
@@ -26,40 +28,13 @@ public abstract class PageBase
         LogPageInfo($"{PageName} is opened");
     }
 
-    public string GetTitle()
-    {
-        var title = browser.WebDriver.Title;
-        log.Information($"Current title - {title}");
-
-        return title;
-    }
-
-    public void NavigateToUrl(string url)
-    {
-        browser.NavigateToUrl(url);
-        log.Information($"Navigating to - {url}");
-    }
-
-    public void GoToTab(int tabIndex)
-    {
-        browser.GoToTab(tabIndex);
-        log.Information($"Going to tab by index '{tabIndex}'");
-    }
-
-    public void CloseCurrentTab()
-    {
-        browser.CloseCurrentTab();
-        log.Information($"Current tab is closed");
-    }
-
-    public void Close()
-    {
-        browser.CloseDriver();
-        log.Information($"Browser is closed");
-    }
-
     protected void LogPageInfo(string logMessage)
     {
-        log.Information($"|{PageName}| {logMessage}");
+        reporter.AddInfo($"|{PageName}| {logMessage}");
+    }
+
+    protected void LogParameterInfo(string paramName, string paramValues)
+    {
+        reporter.AddParameter(paramName, paramValues);
     }
 }
