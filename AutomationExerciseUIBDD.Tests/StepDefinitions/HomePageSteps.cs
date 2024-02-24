0153ac1ab2edae338c890b1e6961d5f4a56a8ca1
Andrey Vlasov <lion32211@yandex.ru>
2024-02-24 12:32:01 +0100

    Add StepDefinitions for first test

---
+++ AutomationExerciseUIBDD.Tests/StepDefinitions/HomePageSteps.cs
@@ -0,0 +1,48 @@
+using AutomationExerciseUIBDD.Tests.Dependencies;
+using AutomationFramework.Common.Models;
+using AutomationFramework.Core.Pages;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AutomationExerciseUIBDD.Tests.StepDefinitions;
+
+[Binding]
+public class HomePageSteps
+{
+	private readonly HomePage homePage;
+	private ScenarioContext scenarioContext;
+
+	public HomePageSteps(ScenarioContext scenarioContext)
+	{
+		this.scenarioContext = scenarioContext;
+		var container = DIContainerSpecflow.GetServiceProvider();
+		homePage = container.GetRequiredService<HomePage>();
+	}
+
+	[When(@"I open home page")]
+	public void OpenHomepage()
+	{
+		homePage.Open();
+	}
+
+	[When(@"I click on delete account header menu")]
+	public void ClickDeleteAccount()
+	{
+		homePage.Header.ClickOnDeleteAccountMenu();
+	}
+
+	[Then(@"I validate home page is opened")]
+	public void ThenIValidateHomePageIsOpened()
+	{
+		homePage.IsPageOpened().Should().BeTrue();
+	}
+
+	[Then(@"I validate Logged in header is visible")]
+	public void ValidateLoggedInHeader()
+	{
+		var user = (User)scenarioContext["user"];
+
+		homePage.Header.GetAllHeadersText()
+			.Any(x => x.Contains($"Logged in as {user.Account.Name}"))
+			.Should().BeTrue();
+	}
+}
